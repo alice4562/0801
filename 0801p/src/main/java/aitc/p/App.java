236eba97cc3cbbe40d9d0dc0edf847e74a80e6d0
alice4562 <chiachi@aitc.com.tw>
2022-08-01 17:56:43 +0800

    0801p2

---
+++ 0801p/src/main/java/aitc/p/App.java
@@ -0,0 +1,26 @@
+package aitc.p;//圖片讀取
+
+import java.io.IOException;
+import java.util.logging.Logger;//導入方法依賴的package包/類
+
+/**
+ * 
+ * @author Azure
+ *
+ */
+
+public class App 
+{
+    private static Logger LOG = Logger.getLogger("App");
+	
+	public static void main( String[] args ) throws IOException
+    {
+		Para p = new Para(args);
+		ImageConvertor newp = new ImageConvertor(p);
+		newp.getconvertor();
+		LOG.info(newp.getimage()?"convert success.":"convert failed.");
+		//LOG.info(new ImageConvertor(p).convertor()?"convert success.":"convert failed.");
+		
+		
+    }
+}
