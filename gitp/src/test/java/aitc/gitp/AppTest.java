c101ac4a4db5c3c5d5b2d9dd8e71bdc75c2e5ed8
alice4562 <chiachi@aitc.com.tw>
2022-08-01 13:40:21 +0800

    0801practice

---
+++ gitp/src/test/java/aitc/gitp/AppTest.java
@@ -0,0 +1,20 @@
+package aitc.gitp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
